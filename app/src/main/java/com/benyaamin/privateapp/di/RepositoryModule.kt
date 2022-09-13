d6d3ade376c2b309a9a39ca18517c737f58a335c
Benyamin Eskandari <me@benyaamin.com>
2022-09-13 13:38:51 +0430

    add RepositoryModule

---
+++ app/src/main/java/com/benyaamin/privateapp/di/RepositoryModule.kt
@@ -0,0 +1,29 @@
+package com.benyaamin.privateapp.di
+
+import com.benyaamin.privateapp.persistence.NoteDao
+import com.benyaamin.privateapp.persistence.PasswordDao
+import com.benyaamin.privateapp.repositories.NoteRepository
+import com.benyaamin.privateapp.repositories.PasswordRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun providePasswordRepository(passwordDao: PasswordDao): PasswordRepository {
+        return PasswordRepository(passwordDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(noteDao: NoteDao): NoteRepository {
+        return NoteRepository(noteDao)
+    }
+
+}
