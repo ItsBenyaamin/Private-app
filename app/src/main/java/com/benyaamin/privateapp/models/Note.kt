fc79bfb4a329095c089b25fed5381e492356291e
Benyamin Eskandari <me@benyaamin.com>
2022-09-13 16:36:40 +0430

    include id to constructor & make entity parcelable

--- app/src/main/java/com/benyaamin/privateapp/models/Note.kt
+++ app/src/main/java/com/benyaamin/privateapp/models/Note.kt
@@ -1,14 +1,16 @@
 package com.benyaamin.privateapp.models
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity
 data class Note(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
     var title: String,
     var content: String,
     var isFavorite: Boolean,
-) {
-    @PrimaryKey(autoGenerate = true)
-    var id: Int = 0
-}
+): Parcelable
